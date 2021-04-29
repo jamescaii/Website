c2f74305c4ff9301b3bbfeeced02a183eec743a0
James Cai <James.cai2780@gmail.com>
2021-04-29 00:37:02 -0400

    ui setup

---
+++ main.js
@@ -0,0 +1,7 @@
+// script for typing animation
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Engineer", "Musician", "Foodie"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
